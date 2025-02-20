90550a021e572941b58c9bf4e3b694d87ec88403
Gustavofontenele1 <gustavofontenele@live.com>
2025-02-20 01:28:57 -0300

    Fix: export

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
@@ -7,3 +9,7 @@ const userSchema = new mongoose.Schema({
   isVerified: { type: Boolean, default: false },
   isEmailSent: { type: Boolean, default: false },
 });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
