cb1951735c72bc28c4a510d96d9865e4a0aba115
Gustavofontenele1 <gustavofontenele@live.com>
2025-02-20 19:33:06 -0300

    Att

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   username: { type: String, required: true },
-  verificationToken: { type: String, required: true },
-  verificationTokenExpiry: { type: Date, required: true },
+  verificationToken: { type: String, required: false },
+  verificationTokenExpiry: { type: Date, required: false },
   isVerified: { type: Boolean, default: false },
   isEmailSent: { type: Boolean, default: false },
 });
